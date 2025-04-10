6e215f1559d0c9dcc10699d559ba0a31c837d32d
harshit <harshitsathone@gmail.com>
2025-04-10 14:11:15 +0530

    patters

---
+++ src/patterns/Pyramid.java
@@ -0,0 +1,20 @@
+package patterns;
+
+public class Pyramid {
+    public static void print(int n){
+        for (int i = 0; i < n; i++) {
+            for (int k=0;k<=(n-i);k++){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i+1; j++) {
+
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        print(5);
+    }
+}
