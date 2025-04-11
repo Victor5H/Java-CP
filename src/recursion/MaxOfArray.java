dee61559d0ff6ffc5a1d6deb9d3c02f59be0ec95
harshit <harshitsathone@gmail.com>
2025-04-11 19:10:58 +0530

    max of array

---
+++ src/recursion/MaxOfArray.java
@@ -0,0 +1,14 @@
+package recursion;
+
+public class MaxOfArray {
+    public static int max(int [] nums,int index){
+        if (index== nums.length) return 0;
+        int got = max(nums,index+1);
+        int max = nums[index]>got ? nums[index]:got;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(max(new int[]{1,2,3,4,5,65,78,7,8,9},7));
+    }
+}
