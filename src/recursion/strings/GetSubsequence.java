ac6815b7b55561ddf4c5e3113682a0f61fe99391
harshit <harshitsathone@gmail.com>
2025-04-13 16:01:21 +0530

    gss

---
+++ src/recursion/strings/GetSubsequence.java
@@ -0,0 +1,27 @@
+package recursion.strings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GetSubsequence {
+    public static ArrayList<String> gss(String str){
+        if(str.length()==0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add("");
+            return list;
+        }
+        char c = str.charAt(0);
+        ArrayList<String> res =gss(str.substring(1));
+        ArrayList<String> sub = new ArrayList<>();
+        for(String s:res){
+            sub.add(""+s);
+            sub.add(c+s);
+        }
+        return sub;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gss("abc"));
+    }
+}
