b8b3cb33315b0fab7bf2e9e3229bd0b4044a23aa
harshit <harshitsathone@gmail.com>
2025-04-19 20:09:52 +0530

    daily

---
+++ src/daily/nineteenApril/Arangram.java
@@ -0,0 +1,23 @@
+package daily.nineteenApril;
+
+import java.util.HashMap;
+
+public class Arangram {
+    public static boolean checkAnagram(String first, String second){
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+        for (int i = 0; i < first.length(); i++) {
+            char  curr = first.charAt(i);
+            map1.put(curr,map1.getOrDefault(curr,0)+1);
+        }
+        for (int i = 0; i < second.length(); i++) {
+            char  curr = second.charAt(i);
+            map2.put(curr,map2.getOrDefault(curr,0)+1);
+        }
+        return map1.equals(map2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkAnagram("listen","silent"));
+    }
+}
---
+++ src/daily/nineteenApril/RotatedAndSorted.java
@@ -0,0 +1,17 @@
+package daily.nineteenApril;
+
+public class RotatedAndSorted {
+    public static boolean sortedAndRotated(int [] nums){
+        int count =0;
+        for (int i = 0; i < nums.length; i++) {
+            int next = nums[(i+1)% nums.length];
+            int curr = nums[i];
+            if(curr>next) count++;
+        }
+        return count<=1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sortedAndRotated(new int[]{4,6,5,1,2,3}));
+    }
+}
