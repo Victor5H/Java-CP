7984914ad0786abd33e150a471df66da063cda8a
harshit <harshitsathone@gmail.com>
2025-04-20 18:36:43 +0530

    pepcoding

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# My CP journey
 ![img.png](img.png)
 
 This is my Git repo to complete my daily coding challenge and leet code practice code, or any other coding practice in Java
---
+++ src/recursion/script.java
@@ -0,0 +1,59 @@
+package recursion;
+
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
+public class script {
+
+    public static void main(String[] args) {
+        String folderPath = "src/recursion/"; // <- 🔁 change this to your Java files directory
+        String indexFile = "index.txt";
+
+        try {
+            // List to store file info with creation time
+            List<FileInfo> fileInfos = new ArrayList<>();
+
+            Files.walk(Paths.get(folderPath))
+                    .filter(path -> path.toString().endsWith(".java"))
+                    .forEach(path -> {
+                        try {
+                            BasicFileAttributes attr = Files.readAttributes(path, BasicFileAttributes.class);
+                            fileInfos.add(new FileInfo(path.getFileName().toString(), attr.creationTime().toMillis()));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+
+            // Sort files by creation time
+            fileInfos.sort(Comparator.comparingLong(f -> f.creationTime));
+
+            // Write to index.txt
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(folderPath+indexFile))) {
+                int counter = 1;
+                for (FileInfo fileInfo : fileInfos) {
+                    if(Objects.equals(fileInfo.name, "script.java")){continue;}
+                    String line = String.format("%03d - %s", counter++, fileInfo.name);
+                    writer.write(line);
+                    writer.newLine();
+                }
+                System.out.println("Index written to " + indexFile);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Helper class
+    static class FileInfo {
+        String name;
+        long creationTime;
+
+        FileInfo(String name, long creationTime) {
+            this.name = name;
+            this.creationTime = creationTime;
+        }
+    }
+}
