8f39d1907f3d47e25ab9a68fc26dc84cc919404e
ubuntu-harshit <harshitsathone@gmail.com>
2025-08-19 07:47:16 +0530

    daily

---
+++ src/daily/eighteenAug/Unique3DigitEvenNumbers.java
@@ -0,0 +1,28 @@
+package daily.eighteenAug;
+
+import java.util.HashSet;
+
+//https://leetcode.com/problems/unique-3-digit-even-numbers/
+// usually it had to return the numbers in a sorted array, but now they are only asking for number of numbers
+public class Unique3DigitEvenNumbers {
+//    unoptimized, brute force;
+    public int totalNumbers(int[] digits) {
+        HashSet<Integer> set = new HashSet<>();
+        int n = digits.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                for(int k=0;k<n;k++){
+                    if(i==j||j==k||i==k) continue;
+                    else{
+                        int num = digits[i]*100+digits[j]*10+digits[k];
+                        if(num>=100 && num%2==0) set.add(num);
+                    }
+                }
+            }
+        }
+        System.out.println(set);
+        return set.size();//returns size, can use treeset to store numbers in a sorted order
+    }
+
+
+}
--- src/twoDArrays/SpiralDisplay.java
+++ src/twoDArrays/SpiralDisplay.java
@@ -17,14 +17,14 @@ public class SpiralDisplay {
     }
 //    https://www.youtube.com/watch?v=SVFXEqn3Ceo
     public static void main(String[] args) {
-        int [][]mat = inputMat();
+        int [][]mat = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}};
         int r = mat.length;
         int c = mat[0].length;
-//        int i = 0,j=0;
-        for (int j = 0;j<mat[0].length;j++){
-            for (int i = 0; i < mat.length; i++) {
-
+        for (int i = 0; i < c/2; i++) {
+            for (int j = 0; j < r / 2; j++) {
+                System.out.print(mat[j][i]+" ");
             }
+            System.out.println();
         }
     }
 
