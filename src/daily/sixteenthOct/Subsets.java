7988b1300200850d7a1c33c47069e97565df934a
ubuntu-harshit <harshitsathone@gmail.com>
2025-10-16 22:50:38 +0530

    pascals triangle

---
+++ src/daily/sixteenthOct/Subsets.java
@@ -0,0 +1,14 @@
+package daily.sixteenthOct;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//https://leetcode.com/problems/subsets/description/
+public class Subsets {
+//    public static List<List<Integer>> subsets(int [] nums, int i, List<Integer> ansSF){
+//        if(i==nums.length-1){
+//
+//        }
+//
+//    }
+}
