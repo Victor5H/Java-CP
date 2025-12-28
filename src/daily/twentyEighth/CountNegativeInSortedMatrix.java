6637671f55b8ec53668dff8e4f99d6ad68889edb
ubuntu-harshit <harshitsathone@gmail.com>
2025-12-28 22:16:27 +0530

    daily missed

--- src/daily/twentyEighth/CountNegativeInSortedMatrix.java
+++ src/daily/twentyEighth/CountNegativeInSortedMatrix.java
@@ -1,4 +1,49 @@
 package daily.twentyEighth;
 
 public class CountNegativeInSortedMatrix {
+    public static int countNegatives(int[][] grid) {
+//        O(mlogN)
+        int l=0,h=grid[0].length-1,mid=0,count=0;
+        for(int i=0;i<grid.length;i++){
+            int []arr = grid[i];
+            l=0;h=grid[0].length-1;
+            if(arr[h]<0){
+                if(arr[l]<0){
+                    count+=(h-l)+1;
+                }
+                else{
+                    while(l<=h){
+                        mid = l+(h-l)/2;
+                        if(arr[mid]<0){
+                            count+=(h-mid)+1;
+                            h=mid-1;
+                        }
+                        else{
+                            l=mid+1;
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+//    O(m+n)
+    public int countNegativesMPlusN(int[][] grid) {
+        int m=grid[0].length,n=grid.length,count=0,i=n-1,j=0;
+        while(i>=0 && j<m){
+            if(grid[i][j]<0){
+                count+=(m-j);
+                i--;
+            }
+            else{
+                j++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int [][]grid = {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
+        System.out.println(countNegatives(grid));
+    }
 }
--- src/javaTopics/concurrency/cf/Main.java
+++ src/javaTopics/concurrency/cf/Main.java
@@ -6,6 +6,6 @@ import java.util.concurrent.ExecutionException;
 public class Main {
     public static void main(String[] args) {
         CompletableFuture<String> f = CompletableFuture.supplyAsync(()->"Harshit");
-        System.out.println(f.join());System.out.println(f.get());
+        System.out.println(f.join());System.out.println(f.join());
     }
 }
