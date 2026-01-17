b4896f036acdad629685d51849d03704fc298e89
ubuntu-harshit <harshitsathone@gmail.com>
2026-01-17 22:37:17 +0530

    search in 2d matrix 2

---
+++ src/daily/TwentySix/seventeenthJan/ProductOfArrayExceptSelf.java
@@ -0,0 +1,25 @@
+package daily.TwentySix.seventeenthJan;
+
+public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+
+        //need to optimize the space
+
+        int n = nums.length;
+        int[] ret = new int[n];
+        int[] pre = new int[n];
+        int[] suf = new int[n];
+        pre[0] = 1;
+        for (int i = 1; i < n; i++) {
+            pre[i] = nums[i - 1] * pre[i - 1];
+        }
+        suf[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            suf[i] = nums[i + 1] * suf[i + 1];
+        }
+        for (int i = 0; i < n; i++) {
+            ret[i] = pre[i] * suf[i];
+        }
+        return ret;
+    }
+}
---
+++ src/daily/TwentySix/seventeenthJan/SearchA2DMatrix2.java
@@ -0,0 +1,18 @@
+package daily.TwentySix.seventeenthJan;
+
+public class SearchA2DMatrix2 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length,m= matrix[0].length;
+        int i=0,j=m-1;
+        while(i<n && j>=0){
+            if(matrix[i][j]==target){
+                return true;
+            }
+            else if(target>matrix[i][j]){
+                i++;
+            }
+            else j--;
+        }
+        return false;
+    }
+}
